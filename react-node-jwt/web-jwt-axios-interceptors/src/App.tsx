99d9cd9fe059d6b18d6ae673bb4759ab1738155d
Ky-Ha <kyjob2611@gmail.com>
2024-08-22 23:13:10 +0700

    feature-Login/JWT: update login page

--- react-node-jwt/web-jwt-axios-interceptors/src/App.tsx
+++ react-node-jwt/web-jwt-axios-interceptors/src/App.tsx
@@ -1,14 +1,17 @@
 import { Routes, Route, Navigate } from "react-router-dom"
+import { CssVarsProvider } from "@mui/joy/styles"
 import Dashboard from "./pages/Dashboard/Dashboard"
 import Login from "./pages/Login/Login"
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" replace={true} />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
+    <CssVarsProvider>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace={true} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </CssVarsProvider>
   )
 }
 
--- react-node-jwt/web-jwt-axios-interceptors/src/pages/Login/Login.tsx
+++ react-node-jwt/web-jwt-axios-interceptors/src/pages/Login/Login.tsx
@@ -1,16 +1,41 @@
-import { CssVarsProvider } from "@mui/joy/styles"
+import * as React from "react"
+import axios from "axios"
+import { toast } from "react-toastify"
+import { useColorScheme } from "@mui/joy/styles"
 import Sheet from "@mui/joy/Sheet"
+import CssBaseline from "@mui/joy/CssBaseline"
 import Typography from "@mui/joy/Typography"
 import FormControl from "@mui/joy/FormControl"
 import FormLabel from "@mui/joy/FormLabel"
 import Input from "@mui/joy/Input"
 import Button from "@mui/joy/Button"
 import Link from "@mui/joy/Link"
-// import { API_ROOT } from "../../utils/constants"
 import { API_ROOT } from "../../utils/constants"
-import { toast } from "react-toastify"
-import "./LoginForm.css"
-import axios from "axios"
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  const [mounted, setMounted] = React.useState(false)
+
+  // necessary for server-side rendering
+  // because mode is undefined on the server
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+  if (!mounted) {
+    return <Button variant="soft">Change mode</Button>
+  }
+
+  return (
+    <Button
+      variant="soft"
+      onClick={() => {
+        setMode(mode === "light" ? "dark" : "light")
+      }}
+    >
+      {mode === "light" ? "Turn dark" : "Turn light"}
+    </Button>
+  )
+}
 
 export default function Login() {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -33,28 +58,38 @@ export default function Login() {
   }
 
   return (
-    <CssVarsProvider>
-      <Sheet
-        sx={{
-          width: 300,
-          mx: "auto", // margin left & right
-          my: 4, // margin top & bottom
-          py: 3, // padding top & bottom
-          px: 2, // padding left & right
-          display: "flex",
-          flexDirection: "column",
-          gap: 2,
-          borderRadius: "sm",
-          boxShadow: "md",
-        }}
-      >
-        <Typography level="h3" sx={{ textAlign: "center", color: "blue" }}>
-          Welcome!
-        </Typography>
-        <Typography level="body-sm" sx={{ textAlign: "center" }}>
-          Sign in to continue.
-        </Typography>
-        <form onSubmit={handleSubmit}>
+    <main>
+      <ModeToggle />
+      <CssBaseline />
+      <form onSubmit={handleSubmit}>
+        <Sheet
+          sx={{
+            width: 300,
+            mx: "auto", // margin left & right
+            my: 4, // margin top & bottom
+            py: 3, // padding top & bottom
+            px: 2, // padding left & right
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            borderRadius: "sm",
+            boxShadow: "md",
+          }}
+          variant="outlined"
+        >
+          <div>
+            <Typography
+              level="h4"
+              component="h1"
+              sx={{ textAlign: "center", color: "blue" }}
+            >
+              <b>Welcome!</b>
+            </Typography>
+            <Typography level="body-sm" sx={{ textAlign: "center" }}>
+              Sign in to continue.
+            </Typography>
+          </div>
+
           <FormControl>
             <FormLabel>Email</FormLabel>
             <Input
@@ -66,20 +101,26 @@ export default function Login() {
           </FormControl>
           <FormControl>
             <FormLabel>Password</FormLabel>
-            <Input name="password" type="password" placeholder="password" />
+            <Input
+              // html input attribute
+              name="password"
+              type="password"
+              placeholder="password"
+            />
           </FormControl>
           <Button type="submit" sx={{ mt: 1 /* margin top */ }}>
             Log in
           </Button>
-        </form>
-        <Typography
-          endDecorator={<Link href="/sign-up">Sign up</Link>}
-          fontSize="sm"
-          sx={{ alignSelf: "center" }}
-        >
-          Don't have an account?
-        </Typography>
-      </Sheet>
-    </CssVarsProvider>
+
+          <Typography
+            endDecorator={<Link href="/sign-up">Sign up</Link>}
+            fontSize="sm"
+            sx={{ alignSelf: "center" }}
+          >
+            Don&apos;t have an account?
+          </Typography>
+        </Sheet>
+      </form>
+    </main>
   )
 }
