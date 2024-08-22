48744e256129872ede361a8fde81a5ea5acfaf96
Ky-Ha <kyjob2611@gmail.com>
2024-08-22 23:38:28 +0700

    JWT/feature-tokenHandle: jwt provider

---
+++ react-node-jwt/api-jwt-axios-interceptors/src/providers/JwtProvider.js
@@ -0,0 +1,20 @@
+import JWT from "jsonwebtoken"
+
+const generateToken = async () => {
+  try {
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const verifyToken = async () => {
+  try {
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+export const JwtProvider = {
+  generateToken,
+  verifyToken,
+}
